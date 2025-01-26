22aa3332412fc4946d3c937de72b8324e36cfc2f
MRABDUL13 <softwaremuller@gmail.com>
2025-01-26 22:16:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-    ALIVE_IMG: process.env.ALIVE_IMG ||""
+    ALIVE_IMG: process.env.ALIVE_IMG ||"https://github.com/MRABDUL13/Muller-MD/blob/main/20250126_220630.jpg"
     ALIVE_IMG: process.evn.ALIVE_IMG || "Hello, I'm Alive
 };
