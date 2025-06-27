3c6b5c34bb07469f76543fef68245b004ab2bf60
SFT MULLER <softwaremuller@gmail.com>
2025-06-27 09:56:52 +0100

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -39,7 +39,7 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363400129137847@newsletter',
+          newsletterJid: '120363419333086422@newsletter',
           newsletterName: 'FACEBOOK DWNLD',
           serverMessageId: 143
         }
